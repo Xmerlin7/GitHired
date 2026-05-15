abb81ecc8aaba44c69cf2ed2a6ccca069eba3b0c
Xmerlin7 <seifahmedjr7@gmail.com>
2026-05-15 19:16:42 +0300

    feat: update users table migration to use enum for role with default value

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('role');
+            $table->enum('role', ['admin', 'employer', 'candidate'])->default('candidate');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
